373eccf45141a9cf0883f31dbc8c5bf9a9e9488e
tangtang1b1b <a0981632653@gmail.com>
2023-07-30 23:49:28 +0800

    [2023.07.30] try firebase

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let sendArea = document.querySelector('#sendArea');
+let typeArea = document.querySelector('#typeArea');
 function send() {
-    let typeArea = document.querySelector('#typeArea');
-    console.log(typeArea.value);
+    if(typeArea.value === '') return
+    textCreate(typeArea.value)
     typeArea.value = '';
 }
-sendArea.onclick = send;
+
+function textCreate(message) {
+    let roomBodyText = document.querySelector('.roomBodyText')
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    div.className = 'textBox mine';
+    p.className = 'words mine';
+    p.innerText = message;
+    div.appendChild(p);
+    roomBodyText.appendChild(div);
+}
+typeArea.addEventListener('keydown',(event) => {
+    if(event.key !== 'Enter') return 
+    send();
+});
