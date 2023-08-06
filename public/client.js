453b228dbf970cb9fb2d0b5402f5d08122ee0e84
tangtang1b1b <a0981632653@gmail.com>
2023-08-06 14:49:02 +0800

    [2023.08.06] local success

--- public/client.js
+++ public/client.js
@@ -5,8 +5,10 @@ let typeArea = document.querySelector('#typeArea');
 
 function send() {
     if(typeArea.value === '') return;
+    let roomBody = document.querySelector('.roomBody');
     let message = typeArea.value;
     textCreate(message, true);
+    roomBody.scrollTop = roomBody.scrollHeight;
     socket.emit('message', message); // 發送訊息到 Socket.io 伺服器
 }
 
@@ -14,17 +16,19 @@ function textCreate(message, isMine) {
     let roomBodyText = document.querySelector('.roomBodyText');
     let div = document.createElement('div');
     let p = document.createElement('p');
-    
+    // let pName = document.createElement('p');
     if(isMine){
         div.className = 'textBox mine';
         p.className = 'words mine';
     } else {
         div.className = 'textBox';
         p.className = 'words';
+        // pName.className = 'userName';
+        // pName.textContent = `${senderId}:`;
     }
-    
     p.innerText = message;
     div.appendChild(p);
+    // div.appendChild(pName);
     roomBodyText.appendChild(div);
     typeArea.value = '';
 }
@@ -33,12 +37,15 @@ typeArea.addEventListener('keydown', (event) => {
     if(event.key !== 'Enter') return;
     send();
 });
+sendArea.addEventListener('click',() => {
+  send();
+});
 
 // 監聽 'message' 事件
-socket.on('message', (data) => {
+socket.on('message', (message) => {
     // console.log('收到訊息：', data);
-    if (data !== typeArea.value) {
-      textCreate(data, false);
+    if (message !== typeArea.value) {
+      textCreate(message, false);
       typeArea.value = '';
     }
 });
--- public/socket.js
+++ public/socket.js
@@ -13,11 +13,11 @@ app.use(express.static(__dirname));
 
 // 監聽連接請求
 io.on('connection', (socket) => {
-  console.log('有客戶端連接成功！');
+  console.log(`有客戶端連接成功!socket ID: ${socket.id}`);
 
   // 監聽 'message' 事件
   socket.on('message', (data) => {
-    console.log('收到訊息：', data);
+    console.log(`收到${socket.id}訊息：`, data);
 
     // 發送訊息給除了發送者之外的所有客戶端
     socket.broadcast.emit('message', data)
@@ -27,5 +27,5 @@ io.on('connection', (socket) => {
 // 啟動伺服器
 const port = 3000;
 server.listen(port, () => {
-  console.log(`伺服器運行在 http://localhost:${port}`);
+  console.log(`伺服器運行在 http://192.168.1.105:${port}`);
 });
